75c2ec29bf576bc90dc10ca0efc1975e58008f4c
otong.sunandar <otong.sunandar@tritronik.com>
2024-07-12 16:45:47 +0700

    update

--- src/main/java/com/klapertart/appversioning/PrintLibraryConst.java
+++ src/main/java/com/klapertart/appversioning/PrintLibraryConst.java
@@ -4,6 +4,8 @@ import com.klapertart.sample.library.utils.Const;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 /**
  * @author tritronik
  * @since 7/10/2024
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class PrintLibraryConst {
+    @PostConstruct
     public void printConst(){
         log.info("CONST VERSI 2: ", Const.CONST_VERSI_002);
     }
